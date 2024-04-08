1feb947571caf1cf8c04834da9fbffa1cb6913f0
godoymariacj <33313243-godoymariacj@users.noreply.replit.com>
2024-04-08 13:25:00 +0000

    Initial commit

---
+++ java/principal_secretario.js
@@ -0,0 +1,7 @@
+function mostrarTelaAvisos() {
+  document.getElementById("telaAvisos").style.display = "block";
+}
+
+function fecharTelaAvisos() {
+  document.getElementById("telaAvisos").style.display = "none";
+}
